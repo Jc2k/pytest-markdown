2af4bab4014396f6401a7df29224731d05ddbfb5
Zach Kanzler <theY4Kman@gmail.com>
2018-07-28 15:03:36 -0400

    Support assertion rewriting and source code display

--- pytest_markdown/plugin.py
+++ pytest_markdown/plugin.py
@@ -1,15 +1,42 @@
-import imp
+import ast
+import types
+from linecache import cache
 
 import CommonMark
 import pytest
+from _pytest.assertion.rewrite import rewrite_asserts
 from _pytest.python import Module
+
+
+def create_pytest_module(name, path, source, config):
+    """Return a Module from source string with its assertions rewritten"""
+    try:
+        tree = ast.parse(source)
+    except SyntaxError:
+        return None
+
+    rewrite_asserts(tree, path, config)
+
+    try:
+        co = compile(tree, path, "exec", dont_inherit=True)
+    except SyntaxError:
+        return
+
+    mod = types.ModuleType(name)
+    exec(co, mod.__dict__)
+
+    # Also, seed the line cache, so pytest can show source code
+    def getsource():
+        return source
+    cache[path] = (getsource,)
+
+    return mod
 
 
 class MarkdownItem(Module):
 
-    def __init__(self, name, file, code, nodeid=None):
-        self._code_obj = imp.new_module(name)
-        exec(code, self._code_obj.__dict__)
+    def __init__(self, name, file, code, nodeid=None, config=None):
+        self._code_obj = create_pytest_module(name, file.name, code, config)
         super().__init__(name, file, nodeid=nodeid)
 
     def _getobj(self):
@@ -88,10 +115,11 @@ class MarkdownCollector(object):
             nodeid = self.stack[-1][1].nodeid + '::' + name
 
         mi = MarkdownItem(
-            name,
-            self.stack[-1][1],
-            output,
-            nodeid=nodeid
+            name=name,
+            file=self.stack[-1][1],
+            code=output,
+            nodeid=nodeid,
+            config=self.item.config,
         )
         self.collected.append(mi)
 
