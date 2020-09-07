04cdab05f20fa65bbcae9c7d11e7e420ad0fa2aa
John Carr <john.carr@unrouted.co.uk>
2020-09-07 12:37:39 +0100

    Move to pytest 6. Apply my standard python setup

--- pytest_markdown/plugin.py
+++ pytest_markdown/plugin.py
@@ -1,12 +1,12 @@
 import ast
-import types
 from linecache import cache
+import types
 
+from _pytest.assertion.rewrite import rewrite_asserts
+from _pytest.python import Module
 import commonmark
 import py
 import pytest
-from _pytest.assertion.rewrite import rewrite_asserts
-from _pytest.python import Module
 
 
 def create_pytest_module(name, path, source, config):
@@ -29,16 +29,18 @@ def create_pytest_module(name, path, source, config):
     # Also, seed the line cache, so pytest can show source code
     def getsource():
         return source
+
     cache[path] = (getsource,)
 
     return mod
 
 
 class MarkdownItem(Module):
-
-    def __init__(self, name, file, code, nodeid=None, config=None):
-        self._code_obj = create_pytest_module(name, file.name, code, config)
-        super().__init__(name, file, nodeid=nodeid)
+    def __init__(self, fspath, parent, code, nodeid=None):
+        self._code_obj = create_pytest_module(
+            fspath.basename, fspath.basename, code, parent.config
+        )
+        super().__init__(fspath, parent, nodeid=nodeid)
 
     def _getobj(self):
         return self._code_obj
@@ -55,33 +57,44 @@ def collect_literals_from_children(token):
             stack.append(cur.nxt)
         if cur.first_child:
             stack.append(cur.first_child)
-    return ''.join(literals)
+    return "".join(literals)
 
 
-class MarkdownCollector(object):
-
+class MarkdownCollector:
     def __init__(self, item):
         super().__init__()
         self.item = item
         self.ast = commonmark.Parser().parse(self.item.fspath.open().read())
-        self.stack = [(0, self.item, self.item.name,)]
+        self.stack = [
+            (
+                0,
+                self.item,
+                self.item.name,
+            )
+        ]
         self.collected = []
 
     def collect(self):
-        self.stack = [(0, self.item, self.item.name,)]
+        self.stack = [
+            (
+                0,
+                self.item,
+                self.item.name,
+            )
+        ]
         self.collected = []
 
         walker = self.ast.walker()
 
         event = walker.nxt()
         while event is not None:
-            if not event['entering']:
+            if not event["entering"]:
                 event = walker.nxt()
                 continue
 
-            func = 'visit_' + event['node'].t
+            func = "visit_" + event["node"].t
             if hasattr(self, func):
-                getattr(self, func)(event['node'])
+                getattr(self, func)(event["node"])
 
             event = walker.nxt()
 
@@ -91,49 +104,48 @@ class MarkdownCollector(object):
         while self.stack[-1][0] >= token.level:
             self.stack.pop()
 
-        name = collect_literals_from_children(token).lower().replace(' ', '-')
-        nodeid = '::'.join(s[2] for s in self.stack) + '::' + name
-        self.stack.append((
-            token.level,
-            pytest.Item(name, self.stack[-1][1], nodeid=nodeid),
-            name
-        ))
+        name = collect_literals_from_children(token).lower().replace(" ", "-")
+        nodeid = "::".join(s[2] for s in self.stack) + "::" + name
+        self.stack.append(
+            (
+                token.level,
+                pytest.Item.from_parent(self.stack[-1][1], name=name, nodeid=nodeid),
+                name,
+            )
+        )
 
     def visit_code_block(self, token):
-        if token.info != 'python':
+        if token.info != "python":
             return
 
         output = token.literal
 
-        if output.startswith('\n'):
-            return ''
+        if output.startswith("\n"):
+            return ""
 
-        name = f'line_{token.sourcepos[0][0]}'
+        name = f"line_{token.sourcepos[0][0]}"
 
-        if output.lower().strip().startswith('# conftest.py\n'):
+        if output.lower().strip().startswith("# conftest.py\n"):
             nodeid = self.stack[-1][1].nodeid
         else:
-            nodeid = self.stack[-1][1].nodeid + '::' + name
+            nodeid = self.stack[-1][1].nodeid + "::" + name
 
-        mi = MarkdownItem(
-            name=name,
-            file=self.stack[-1][1],
+        mi = MarkdownItem.from_parent(
+            self.stack[-1][1],
+            fspath=self.item.fspath,
             code=output,
             nodeid=nodeid,
-            config=self.item.config,
         )
         self.collected.append(mi)
 
-        return ''
+        return ""
 
 
 class MarkdownFile(pytest.File):
-
     def collect(self):
-        for item in MarkdownCollector(self).collect():
-            yield item
+        yield from MarkdownCollector(self).collect()
 
 
 def pytest_collect_file(parent, path):
-    if path.ext == '.md':
-        return MarkdownFile(path, parent)
+    if path.ext == ".md":
+        return MarkdownFile.from_parent(parent, fspath=path)
